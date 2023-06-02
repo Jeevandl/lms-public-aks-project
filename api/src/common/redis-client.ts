520ae628da0f907bf8e3fc1893acc26a7bb69696
Ramesh <ramesh-km@outlook.com>
2023-06-02 21:50:32 +0530

    feat: modify redis client to support unencrypted connections

--- api/src/common/redis-client.ts
+++ api/src/common/redis-client.ts
@@ -2,8 +2,8 @@ import { createClient } from "redis";
 import { getConfig } from "./config";
 
 const redisClient = createClient({
-  url: `rediss://${getConfig("REDIS_HOST")}`,
-  password: getConfig("REDIS_PASSWORD") as string,
+  url: `redis://${getConfig("REDIS_HOST")}`,
+  password: getConfig("REDIS_PASSWORD") ? getConfig("REDIS_PASSWORD") : undefined,
 });
 
 export default redisClient;
