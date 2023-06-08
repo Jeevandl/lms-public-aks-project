e065a00149a70046449552f195bd1cd50eb34ba0
Ramesh <58159495+ramesh-km@users.noreply.github.com>
2023-06-08 11:38:05 +0530

    Update redis-client.ts

--- api/src/common/redis-client.ts
+++ api/src/common/redis-client.ts
@@ -2,7 +2,7 @@ import { createClient } from "redis";
 import { getConfig } from "./config";
 
 const redisClient = createClient({
-  url: `redis://${getConfig("REDIS_HOST")}`,
+  url: `rediss://${getConfig("REDIS_HOST")}`,
   password: getConfig("REDIS_PASSWORD") ? getConfig("REDIS_PASSWORD") : undefined,
 });
 
